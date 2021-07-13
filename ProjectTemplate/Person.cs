6d26d24dbb45a3b6b7933fd153d971f01e36a166
jackxhawkins13 <jackxhawkins13@gmail.com>
2021-07-12 20:14:51 -0700

    Initial Setup

---
+++ ProjectTemplate/Person.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectTemplate
+{
+    public class Person 
+    {
+        public string firstName;
+        public string lastName;
+        public int phoneNumber;
+    }
+}
